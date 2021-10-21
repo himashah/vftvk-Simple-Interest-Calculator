8cf793bc12a80bae5b3fda81809a556f52aaf4d3
himashah <himani.shah711@gmail.com>
2021-10-21 15:08:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    result = (p*r*y)/100
 }
         
