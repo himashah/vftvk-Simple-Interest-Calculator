842b833abc6268b95eca6910b49062342fa55ce5
himashah <himani.shah711@gmail.com>
2021-10-27 23:23:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+	if(p=='' || p<=0) {
+		var res = document.getElementById("res");
+		res.innerHTML = "";
+        alert("Invalid input. Please enter positive number")
+		document.getElementById("principal").focus();
+		
+		return;
+
+    } 
+		
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
 	var date = new Date(); 
     cur = date.getFullYear() + Number(y);
 
-    result = Number(p) + (p*r*y)/100
+    result = (p*r*y)/100
 	
 	var res = document.getElementById("res");
 	res.innerHTML = 'If you deposit <mark>'+ p + '</mark> <br/> at an interest rate of <mark>'+ r +'% </mark><br/>You will recieve an amount of <mark>' + result + '</mark><br/>' + 'in the year <mark>' + cur +'</mark>';
 	
 }
-
         
