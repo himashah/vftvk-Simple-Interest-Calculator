39d039bc41c28ca71c1679a25e601c92a3f33911
himashah <himani.shah711@gmail.com>
2021-10-27 22:12:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function compute()
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
-    result = (p*r*y)/100
+	var date = new Date(); 
+    cur = date.getFullYear() + Number(y);
+
+    result = Number(p) + (p*r*y)/100
+	
+	var res = document.getElementById("res");
+	res.innerHTML = 'If you deposit <mark>'+ p + '</mark> <br/> at an interest rate of <mark>'+ r +'% </mark><br/>You will recieve an amount of <mark>' + result + '</mark><br/>' + 'in the year <mark>' + cur +'</mark>';
+	
 }
+
         
